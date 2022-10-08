0b4e2fd6e28ba224ec16a5ebc01c79feaa7ed0cc
Onoriode Dafetta <onoriodedafetta@gmail.com>
2022-10-08 17:24:42 +0100

    minor fixes on responsiveness

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -19,7 +19,7 @@ const Discover = () => {
   const genreTitle = genres.find(({ value }) => value === genreListId)?.title;
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-full">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
         <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      screens: {
+        xs: '375px',
+        sm: '600px',
+      },
       colors: {
         black: '#191624',
       },
